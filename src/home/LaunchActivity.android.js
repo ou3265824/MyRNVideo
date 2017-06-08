315061e8ee32a5cda26d0759c2d3141f38ae209a
olq <632521056@qq.com>
2017-06-08 18:14:59 +0800

    请求数据

--- src/home/LaunchActivity.android.js
+++ src/home/LaunchActivity.android.js
@@ -2,14 +2,33 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet,
     TouchableHighlight,
-    
+    TextInput,
+    Image,
+    ToastAndroid,
  } from 'react-native';
 
+
+
+import NetUtils from '../utils/NetUtils';
 
 import {StackNavigator} from 'react-navigation'
 
 // create a component
 class Launch extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            userName:'',
+            pass:'',
+        };
+    }
+
+    login(){
+        NetUtils.getUser(this.state.userName,this.state.pass,function (set) {
+            ToastAndroid.show(JSON.stringify(set),ToastAndroid.SHORT)
+        })
+    }
 
     static navigationOptions={
         header:null,
@@ -19,8 +38,23 @@ class Launch extends Component {
         const {navigate}=this.props.navigation;
         return (
             <View style={styles.container}>
+                <Image source={require('../images/refresh_failed.png')} />
+                <View>
+                    <Text>a</Text>
+                    <TextInput placeholder={'开始'}
+                        style={{height: 40, width:'40%', borderColor: 'gray', padding: 0,borderWidth: 1}}
+                               onChangeText={(userName)=>this.setState({userName})} value={this.state.userName}/>
+                </View>
+                <View>
+                    <Text>b</Text>
+                    <TextInput placeholder={'结束'} onChangeText={(pass)=>this.setState({pass})} value={this.state.pass}/>
+                </View>
                 <TouchableHighlight onPress={
-                    ()=>navigate('Home')
+                    // ()=>navigate('Home')
+                    ()=>{
+                        this.login()
+                        // ToastAndroid.show(this.state.userName+'---'+this.state.pass,ToastAndroid.LONG);
+                    }
                 }>
                     <Text>Launch 开始</Text>
                 </TouchableHighlight>
